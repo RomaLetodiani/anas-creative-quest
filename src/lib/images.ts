a294ee2e133d4ee479dac15e6c07f714bb73132c
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-07-19 14:34:40 +0400

    Refactor project structure and update README. Removed unused SVG files and added new image assets. Introduced HomePageView component for better organization. Updated layout and metadata for the application.

---
+++ src/lib/images.ts
@@ -0,0 +1,155 @@
+type ImagePath = `/images/img${number}.jpeg`;
+
+type Challenge = {
+  id: number;
+  name: string;
+  images: ImagePath[];
+  linkedin: string;
+};
+
+export const challenges: Challenge[] = [
+  {
+    id: 1,
+    name: "The Hidden Forrest",
+    images: ["/images/img1.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-activity-7345392404914397186-yaEk",
+  },
+  {
+    id: 2,
+    name: "Last Breath of The Earth",
+    images: ["/images/img2.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-canvaai-activity-7345752211592810498-31EQ",
+  },
+  {
+    id: 3,
+    name: "The Dreamer's Library",
+    images: ["/images/img3.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-canvaai-activity-7346133299167178752-2nMo",
+  },
+  {
+    id: 4,
+    name: "Rebuild from Trash",
+    images: ["/images/img4.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-activity-7346610458830721024-ppsK",
+  },
+  {
+    id: 5,
+    name: "The Nomad Cities",
+    images: ["/images/img5.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%AE%E1%83%A3%E1%83%97%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-the-nomad-cities-activity-7346959692788592640-HYUO",
+  },
+  {
+    id: 6,
+    name: "Tiny Forrest of My Dreams",
+    images: ["/images/img6.jpeg", "/images/img7.jpeg", "/images/img8.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-activity-7347178012578144256-BiKu",
+  },
+  {
+    id: 7,
+    name: "Inner Peace",
+    images: ["/images/img9.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-activity-7347519470531137536-ozBA",
+  },
+  {
+    id: 8,
+    name: "A Tale of a Secret Town",
+    images: [
+      "/images/img10.jpeg",
+      "/images/img11.jpeg",
+      "/images/img12.jpeg",
+      "/images/img13.jpeg",
+      "/images/img14.jpeg",
+    ],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-activity-7347952704410808320-jO4z",
+  },
+  {
+    id: 9,
+    name: "World at Your Feet",
+    images: [
+      "/images/img15.jpeg",
+      "/images/img16.jpeg",
+      "/images/img17.jpeg",
+      "/images/img18.jpeg",
+    ],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-activity-7348380185068539904-X_AJ",
+  },
+  {
+    id: 10,
+    name: "The Forbidden Bookshop",
+    images: ["/images/img19.jpeg", "/images/img20.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-activity-7348653023859982336-djHS",
+  },
+  {
+    id: 11,
+    name: "My Tiny Corner",
+    images: ["/images/img21.jpeg", "/images/img22.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_madewithcanva-activity-7349122645956640768-ZKI-",
+  },
+  {
+    id: 12,
+    name: "The Portal",
+    images: ["/images/img23.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%97%E1%83%9D%E1%83%A0%E1%83%9B%E1%83%94%E1%83%A2%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-the-portal-activity-7349499082240851968-fYAJ",
+  },
+  {
+    id: 13,
+    name: "A Jar Full of Ideas",
+    images: ["/images/img24.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%AA%E1%83%90%E1%83%9B%E1%83%94%E1%83%A2%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-a-jar-full-of-activity-7349884790805860354-dZbb",
+  },
+  {
+    id: 14,
+    name: "A Magical Friendship",
+    images: ["/images/img25.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%97%E1%83%9D%E1%83%97%E1%83%AE%E1%83%9B%E1%83%94%E1%83%A2%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-a-magical-friendship-activity-7350244324346900481-mBXw",
+  },
+  {
+    id: 15,
+    name: "Portrait of a Dreamer",
+    images: ["/images/img26.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%97%E1%83%AE%E1%83%A3%E1%83%A2%E1%83%9B%E1%83%94%E1%83%A2%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-portrait-of-a-activity-7350576500758269953-wyKT",
+  },
+  {
+    id: 16,
+    name: "Dream Factory",
+    images: ["/images/img27.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%97%E1%83%94%E1%83%A5%E1%83%95%E1%83%A1%E1%83%9B%E1%83%94%E1%83%A2%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-dream-factory-activity-7350920136251322371-W6tw",
+  },
+  {
+    id: 17,
+    name: "The Tree of Dreams",
+    images: ["/images/img28.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%A9%E1%83%95%E1%83%98%E1%83%93%E1%83%9B%E1%83%94%E1%83%A2%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-the-tree-of-activity-7351312001647677443-cLum",
+  },
+  {
+    id: 18,
+    name: "Lantern Festival",
+    images: ["/images/img29.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%97%E1%83%95%E1%83%A0%E1%83%90%E1%83%9B%E1%83%94%E1%83%A2%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-lantern-festival-activity-7351660234647515136-jkwU",
+  },
+  {
+    id: 19,
+    name: "The Umbrella Orchard",
+    images: ["/images/img30.jpeg", "/images/img31.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%AA%E1%83%AE%E1%83%A0%E1%83%90%E1%83%9B%E1%83%94%E1%83%A2%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-the-umbrella-activity-7352018216505819136-gA_a",
+  },
+];
