580905617eda940ed03647ad87a64ce81dffc9e1
Roman Letodiani <136558219+RomaLetodiani@users.noreply.github.com>
2025-07-20 22:23:35 +0400

    Update images.data.ts

--- src/lib/images.data.ts
+++ src/lib/images.data.ts
@@ -172,6 +172,14 @@ export const challenges: Challenge[] = [
     linkedin:
       "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9B%E1%83%94%E1%83%AA%E1%83%AE%E1%83%A0%E1%83%90%E1%83%9B%E1%83%94%E1%83%A2%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-the-umbrella-activity-7352018216505819136-gA_a",
   },
+  {
+    id: 20,
+    name: "The Secret Night Market",
+    slug: "the-secret-night-market",
+    images: ["/images/img32.jpeg"],
+    linkedin:
+      "https://www.linkedin.com/posts/anakopadze_%E1%83%92%E1%83%90%E1%83%9B%E1%83%9D%E1%83%AC%E1%83%95%E1%83%94%E1%83%95%E1%83%98%E1%83%A1-%E1%83%9D%E1%83%AA%E1%83%93%E1%83%90%E1%83%9B%E1%83%94%E1%83%94%E1%83%A0%E1%83%97%E1%83%94-%E1%83%93%E1%83%A6%E1%83%94-the-secret-night-activity-7352726337838637057-9oQU",
+  },
 ];
 
 export const challengesMap = new Map(
